25bbb42f703c6710190ad21bc999c889d957ecff
Krystle <krystle@Krystles-MBP.lan>
2024-06-16 10:30:24 -0700

    Quiz questions and answers are rendering to the DOM

--- vote/src/components/quiz/Quiz.js
+++ vote/src/components/quiz/Quiz.js
@@ -1,13 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import DOMPurify from 'dompurify';
-//
+import style from './quiz.css'
 
 
 const Quiz = () => {
     const [quiz, setQuiz] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [activeQuestion, setActiveQuestion] = useState(0);
     
 
     useEffect(() => {
@@ -23,7 +24,36 @@ const Quiz = () => {
                 const html = response.data
                 const sanitizedHtml = DOMPurify.sanitize(html)
 
-                setQuiz(sanitizedHtml)
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, 'text/html');
+                
+                // // Select the specific element
+                const specificElement = doc.querySelector('#content .ui-hidden dl');
+                // console.log(specificElement)
+
+                // Select all <dl> elements
+                const dls = doc.querySelectorAll('#content .ui-hidden dl');
+                console.log('dls',dls)
+
+                // // Initialize an empty array to hold the questions and answers
+                const qaArray = [];
+
+                // Loop through each <dl> element
+                dls.forEach(dl => {
+                // Get the <dt> and <dd> elements
+                const dt = dl.querySelector('dt');
+                const dd = dl.querySelector('dd');
+
+                // Extract the text content and remove the "Question: " and "Answer: " prefixes
+                const question = dt.textContent.replace('Question: ', '').trim();
+                const answer = dd.textContent.replace('Answer: ', '').trim();
+
+                // Add the question and answer to the array as an object
+                qaArray.push({ question, answer });
+                });
+                setQuiz(qaArray)
+
+
                 setLoading(false)
 
             } catch (error) {
@@ -51,12 +81,20 @@ const Quiz = () => {
 
    
 
-  
+  console.log('quizState',quiz)
 
   return (
     <div className='quiz-container'>
-          <div  dangerouslySetInnerHTML={{ __html: quiz }} />
+        <button onClick={() => setActiveQuestion(prev => prev - 1 < 0 ? 0 : prev - 1)}>Prev Question</button>
+         {quiz && quiz.map((quiz,index) => {
+            return <div className={`question ${activeQuestion === index ? 'activeQuestion' : null}`}>
+                <span>Question: {quiz.question}</span>  
+                <div> <span>Answer: {quiz.answer}</span></div> 
+            </div>
+            
+         })}
          
+        <button onClick={() => setActiveQuestion(prev => prev + 1 > quiz.length - 1 ? prev : prev + 1)} >Next Question</button>
     </div>
   )
 }
